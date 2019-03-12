7e01def6b517f528d2662e2b5db08c1a87336bc4
Jorge Espinar <jor0veg@gmail.com>
2019-03-12 12:42:11 +0100

    Update execSonar.sh

--- execSonar.sh
+++ execSonar.sh
@@ -1,5 +1,1 @@
-./gradlew sonarqube \            
-  -Dsonar.projectKey=cobramarley_SpaceInvaders \
-  -Dsonar.organization=cobramarley-github \
-  -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=3b6642f9d78ed98db8eafb9349a0192ad62766a2
+./gradlew sonarqube -Dsonar.projectKey=cobramarley_SpaceInvaders -Dsonar.organization=cobramarley-github -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=3b6642f9d78ed98db8eafb9349a0192ad62766a2
