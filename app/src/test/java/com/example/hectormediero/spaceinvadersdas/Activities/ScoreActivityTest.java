6825d7c2510a5b2a410c31f9007ce3677e2d7e50
Jorge Espinar <jor0veg@gmail.com>
2019-03-25 14:37:41 +0100

    se intenta

---
+++ app/src/test/java/com/example/hectormediero/spaceinvadersdas/Activities/ScoreActivityTest.java
@@ -0,0 +1,33 @@
+package com.example.hectormediero.spaceinvadersdas.Activities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ScoreActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void doItIfNotEqual() {
+    }
+
+    @Test
+    public void onPause() {
+    }
+
+    @Test
+    public void onActivityResult() {
+    }
+
+    @Test
+    public void onBackPressed() {
+    }
+}
